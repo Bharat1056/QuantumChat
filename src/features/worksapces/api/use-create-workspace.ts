8507f27c4a4dcc6e83af07ae2903165d91a055fe
bharat <bharatpanigrahi225@gmail.com>
2024-09-08 23:22:30 +0530

    update: createworkspace hook

--- src/features/worksapces/api/use-create-workspace.ts
+++ src/features/worksapces/api/use-create-workspace.ts
@@ -1,11 +1,12 @@
-import { useMutation } from "convex/react";
+"use client";
 
+import { useMutation } from "convex/react";
 import { api } from "../../../../convex/_generated/api";
-import { useCallback } from "react";
+import { useCallback, useMemo, useState } from "react";
 import { Id } from "../../../../convex/_generated/dataModel";
 
 type RequestType = { name: string };
-type ResponseType = Id<"workspaces">;
+type ResponseType = Id<"workspaces"> | null;
 
 type Options = {
   onSuccess?: (data: ResponseType) => void;
@@ -15,17 +16,32 @@ type Options = {
 };
 
 export const useCreateWorkspace = () => {
+  const [data, setData] = useState<ResponseType>(null);
+  const [error, setError] = useState<Error | null>(null);
+  const [status, setStatus] = useState<"success" | "error" | "pending" | "settled" | null>(null)
+
+  const isPending = useMemo(() => status === "pending", [status]);
+  const isError = useMemo(() => status === "error", [status]);
+  const isSuccess = useMemo(() => status === "success", [status]);
+  const isSettled = useMemo(() => status === "settled", [status]);
+
   const mutation = useMutation(api.workspaces.create);
 
   const mutate = useCallback(
     async (values: RequestType, options?: Options) => {
       try {
+        setData(null);
+        setError(null);
+        setStatus("pending")
+
         const response = await mutation(values);
+
         options?.onSuccess?.(response);
       } catch (error) {
         options?.onError?.(error as Error);
         if (options?.throwError) throw error;
       } finally {
+        setStatus("settled")
         options?.onSettled?.();
       }
     },
@@ -33,5 +49,11 @@ export const useCreateWorkspace = () => {
   );
   return {
     mutate,
+    isPending,
+    isError,
+    isSuccess,
+    isSettled,
+    data,
+    error,
   };
 };
